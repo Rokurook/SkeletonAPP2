00da1c4ce427fa8295daa3d4e822812d6f81e4c1
Rokurook <ma.requena@duocuc.cl>
2025-11-05 00:07:27 -0300

    validación y interpolación de datos

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router'; //comunicar pagina con otra / incluir constructor como atributo privado 
 
 @Component({
   selector: 'app-login',
@@ -11,12 +13,54 @@ export class LoginPage  {
   email:string = '';
   password: string='';
 
-  constructor() { }
+  constructor(private alertController: AlertController,
+              private router: Router
+  ) { }
 
-/*función para validar el formato del email*/
+  //funsión necesita libreria / ademas de definir como objeto privado dentro del contructor / Método para mostrar alerta de error
+  async mostrarAlerta(mensaje: string){
+    const alert = await this.alertController.create({
+      header :'Error',
+      message: mensaje,
+      buttons: ['OK']
+    }); 
+    await alert.present();
+  }
+
+
+//función para validar el formato del email
 validarEmail(email: string): boolean {
-  const patron = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; /*Expresión para validar email*/
-  return patron.test(email);
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //Expresión para validar email
+  return emailRegex.test(email);
 }
+
+  login(){
+    //verificar que campo de correo no esté vacio
+    if (!this.email){
+      this.mostrarAlerta('El campo de correo no puede estar vacio.');
+      return;
+    }
+
+    //validar el formato del correo 
+    if(!this.validarEmail(this.email)) {
+      this.mostrarAlerta('El formato del correo es invalido.');
+      return;
+    }
+
+    //verificar que contraseña no esté vacia 
+    if(!this.password){
+      this.mostrarAlerta('El campo de contraseña no puede estar vacio.');
+      return;
+    }
+
+    //Verificar que la contraseña tenga máximo 4 caracteres 
+    if(this.password.length < 6) {
+      this.mostrarAlerta('El campo de contraseña debe tener minimo 6 caracteres.');
+      return;
+    }
+
+    //si todas las validadciones son correctas, navega a la pagina "home"
+    this.router.navigate(['/home'],{ state:{ user: this.email}});
+  }
 
 }
